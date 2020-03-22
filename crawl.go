2eae9a3aec894f92aa9c87fbd152d5636444cae9
ayyjohn <alecjohnson55@gmail.com>
2020-03-22 10:20:54 -0700

    refactor methods to increase readability

--- crawl.go
+++ crawl.go
@@ -7,19 +7,42 @@ import (
 )
 
 func main() {
-	flag.Parse()
-	args := flag.Args()
+	args := parseArgs()
 
-	if len(args) < 1 {
-		fmt.Println("please specify a page to start on")
-		os.Exit(1)
-	} else if len(args) > 1 {
-		fmt.Println("please only specify one page to start on")
+	queue := make(chan string)
+
+	go func() {
+		queue <- args[0]
+	}()
+
+	for uri := range queue {
+		enqueueLinks(uri, queue)
 	}
+}
 
-	retrieve(args[0])
+func enqueueLinks(uri string, queue chan string) {
+	fmt.Println("fetching", uri)
+	links := ScrapeLinks(uri)
+	for _, link := range links {
+		go func(l string) { queue <- l }(link)
+	}
 }
 
 func retrieve(uri string) {
 	fmt.Println(ScrapeLinks(uri))
+}
+
+func parseArgs() []string {
+	flag.Parse()
+	args := flag.Args()
+
+	validateArgs(args)
+	return args
+}
+
+func validateArgs(args []string) {
+	if len(args) != 1 {
+		fmt.Println("please specify exactly one page to start on")
+		os.Exit(1)
+	}
 }
