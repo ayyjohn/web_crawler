bd88416757004b9b6dab0202ee149f5c6206e621
ayyjohn <alecjohnson55@gmail.com>
2020-03-21 20:00:53 -0700

    add minimal arg parsing

--- crawl.go
+++ crawl.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("I'm a gopher!")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 1 {
+		fmt.Println("please specify a page to start on")
+		os.Exit(1)
+	} else {
+		for _, arg := range args {
+			fmt.Println(arg)
+		}
+	}
 }
