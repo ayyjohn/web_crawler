ec55dcb4df1d511cdcb660d01e0658c3a5c11ae4
ayyjohn <alecjohnson55@gmail.com>
2020-03-21 20:08:19 -0700

    improve error handling

--- crawl.go
+++ crawl.go
@@ -13,9 +13,11 @@ func main() {
 	if len(args) < 1 {
 		fmt.Println("please specify a page to start on")
 		os.Exit(1)
-	} else {
+	} else if len(args) == 1 {
 		for _, arg := range args {
 			fmt.Println(arg)
 		}
+	} else {
+		fmt.Println("please only specify one page to start on")
 	}
 }
