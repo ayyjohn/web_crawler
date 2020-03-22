2b46cb1242f3b74914734502687233f7595353a1
ayyjohn <alecjohnson55@gmail.com>
2020-03-22 10:19:17 -0700

    enable handling faulty links

--- util.go
+++ util.go
@@ -7,7 +7,10 @@ import (
 // ScrapeLinks parses all hrefs from a url and return them in a slice
 func ScrapeLinks(uri string) []string {
 	var links []string
-	doc, _ := goquery.NewDocument(uri)
+	doc, err := goquery.NewDocument(uri)
+	if err != nil {
+		return make([]string, 0)
+	}
 	doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
 		href, _ := item.Attr("href")
 		links = append(links, href)
