3e65155da934cadf5f5d9b6b121e4d98b29b6860
ayyjohn <alecjohnson55@gmail.com>
2020-03-22 11:12:12 -0700

    ensure links are only visited once

--- crawl.go
+++ crawl.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+var visitedURLs = make(map[string]bool)
+
 func main() {
 	args := parseArgs()
 
@@ -22,9 +24,16 @@ func main() {
 
 func enqueueLinks(uri string, queue chan string) {
 	fmt.Println("fetching", uri)
+	visitedURLs[uri] = true
+	if len(visitedURLs)%25 == 0 {
+		fmt.Printf("fetched %v links\n", len(visitedURLs))
+	}
 	links := ScrapeLinks(uri)
 	for _, link := range links {
-		go func(l string) { queue <- l }(link)
+		absoluteLink := FixURL(link, uri)
+		if uri != "" && !visitedURLs[absoluteLink] {
+			go func(l string) { queue <- l }(absoluteLink)
+		}
 	}
 }
 
